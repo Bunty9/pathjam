788ef6a66c2be5e4ecad5397a0403571d5dd62e6
Bipin C <63953352+Bunty9@users.noreply.github.com>
2025-01-21 23:41:04 +0530

    cleanup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,31 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		screens: {
+			sm: "375px",
+			md: "768px",
+			lg: "1200px",
+		},
+		extend: {
+			container: {
+				center: true,
+				padding: {
+					DEFAULT: "0px",
+					lg: "80px",
+					md: "70px",
+				},
+			},
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
